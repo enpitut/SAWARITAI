ea87bc83d62cf5299d766db5f3479633ff87df6c
harperfu6 <beharp8a8@gmail.com>
2015-08-22 18:13:35 +0900

    update Arduino file

--- photo_reflecter/photo_reflecter.ino
+++ photo_reflecter/photo_reflecter.ino
@@ -11,44 +11,43 @@ unsigned long span = 300.;//次のINの認識まで何ミリ秒待つか
 boolean recognize = true;//今INを認識する状態か否か
 
 void setup() {
-//Serial.begin(9600);
-Serial.begin(38400);
-Serial.println("start");
+  //Serial.begin(9600);
+  Serial.begin(38400);
+  Serial.println("start");
 
-for(int i = 0; i < sensorNum; i++){
-  analogPins[i] = i;
-  firstVal[i] = analogRead(analogPins[i]);
-  
+  for(int i = 0; i < sensorNum; i++){
+    analogPins[i] = i;
+    firstVal[i] = analogRead(analogPins[i]);  
+  }
 }
 
-}
 void loop() {
 
-for(int i = 0; i < sensorNum; i++){
-  val[i] = analogRead(analogPins[i]) ; //アナログ0番ピンからセンサ値を読み込み
-}
+  for(int i = 0; i < sensorNum; i++){
+    val[i] = analogRead(analogPins[i]) ; //アナログ0番ピンからセンサ値を読み込み
+  }
 
-for(int i = 0; i < sensorNum; i++){
-  //Serial.print(i); // シリアルモニターへ表示
-  //Serial.print(": "); // シリアルモニターへ表示
-  //Serial.println(val[i]) ; // シリアルモニターへ表示
+  for(int i = 0; i < sensorNum; i++){
+    //Serial.print(i); // シリアルモニターへ表示
+    //Serial.print(": "); // シリアルモニターへ表示
+    //Serial.println(val[i]) ; // シリアルモニターへ表示
   
-  if(val[i] < firstVal[i] - recDiff){
-    if(recognize){
-      if(i == 0){
-        Serial.println("BOTTLE IN!!!!!!") ; // シリアルモニターへ表示
+    if(val[i] < firstVal[i] - recDiff){
+      if(recognize){
+        if(i == 0){
+          Serial.println("BOTTLE IN!!!!!!") ; // シリアルモニターへ表示(ボトル)
+        }
+        if(i == 1){
+          Serial.println("CAP IN!!!!!!") ; // シリアルモニターへ表示(ボトル)
+        }
+        startTime = millis();
+        recognize = false;
       }
-      if(i == 1){
-        Serial.println("CAP IN!!!!!!") ; // シリアルモニターへ表示
-      }
-      startTime = millis();
-      recognize = false;
+    }
+    if(millis() - startTime > span){
+      recognize = true;
     }
   }
-  if(millis() - startTime > span){
-    recognize = true;
-  }
-}
 
-delay(1); // 100ms待つ
+  delay(1); // 1ms待つ
 }
