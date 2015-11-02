d406a3c981832117daf86ac8fc6ffb7104669c9d
koooootake <koooootake@koooootake-MacBookPro.local>
2015-11-02 18:54:08 +0900

    Change View
    Viewを切り替え
    calendarの修正

--- iOS/PoiPet/AppDelegate.swift
+++ iOS/PoiPet/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+/*
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
         let pageViewController: ViewController = ViewController()
@@ -25,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
+*/
         return true
     }
 
---
+++ iOS/PoiPet/MainViewController.swift
@@ -0,0 +1,387 @@
+//
+//  MainViewController.swift
+//  
+//
+//  Created by koooootake on 2015/11/02.
+//
+//
+
+import UIKit
+
+class MainViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,NSXMLParserDelegate {
+    
+    var calendarCollectionView : UICollectionView!
+    
+    var currentYear:Int=2015
+    var currentMonth:Int=1
+    var currentDay:Int=1
+    var currentMonthLastDay:Int=1
+    
+    var todayPoi:Int=0
+    
+    var wBounds:CGFloat=0.0
+    var hBounds:CGFloat=0.0
+    
+    var hNavigation:CGFloat=0.0
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        wBounds = self.view.bounds.width
+        hBounds = self.view.bounds.height*2/3
+        
+        //ナビゲーションバー
+        let titleImageView = UIImageView( image: UIImage(named: "logoWhite.png"))
+        titleImageView.contentMode = .ScaleAspectFit
+        titleImageView.frame = CGRectMake(0, 0, self.view.frame.width/2, self.navigationController!.navigationBar.frame.height)
+        self.navigationItem.titleView = titleImageView
+        //ボタン色
+        self.navigationController!.navigationBar.tintColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        //背景色
+        self.navigationController!.navigationBar.barTintColor = UIColor(red: 237.0/255.0, green: 125.0/255.0, blue: 49.0/255.0, alpha: 1.0)//オレンジ
+        
+        hNavigation = CGFloat(self.navigationController!.navigationBar.frame.height) + CGFloat(UIApplication.sharedApplication().statusBarFrame.height)
+        
+        //Poiくんgif追加
+        let backgroundAnimationImage=FLAnimatedImageView()
+        let path = NSBundle.mainBundle().pathForResource("poikun", ofType: "gif")!
+        let url = NSURL(fileURLWithPath: path)
+        let animatedImage = FLAnimatedImage(animatedGIFData: NSData(contentsOfURL: url))
+        backgroundAnimationImage.animatedImage = animatedImage
+        backgroundAnimationImage.frame=CGRectMake(0.0, self.view.frame.height*2/3, self.view.frame.width, self.view.frame.height*1/3)
+        self.view.addSubview(backgroundAnimationImage)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //カレンダービュー追加
+        self.view.addSubview(calendarView())
+        //self.view.addSubview(todayView())
+    }
+    
+    //カレンダーView
+    private func calendarView() -> UIView{
+        
+        //現在の月と年と日を取得
+        let dateFormatter:NSDateFormatter = NSDateFormatter();
+        dateFormatter.dateFormat = "yyyy/MM/dd";
+        let dateString:String = dateFormatter.stringFromDate(NSDate());
+        var dates:[String] = dateString.componentsSeparatedByString("/")
+        
+        currentYear  = Int(dates[0])!
+        currentMonth = Int(dates[1])!
+        currentDay = Int(dates[2])!
+        currentMonthLastDay=getLastDay(currentYear, month: currentMonth)
+        
+        //セルのレイアウト
+        let layout = UICollectionViewFlowLayout()
+        //1つのcellのサイズ
+        let space:CGFloat=3
+        //viewの縦横比に対応して配置を変更
+        //w20:横スペース　w50:ヘッダースペース
+        if wBounds-20 < hBounds-50{
+            layout.itemSize=CGSizeMake(((wBounds-20)-space*8)/7, ((wBounds-20)-space*8)/7)
+            layout.minimumInteritemSpacing=space
+            layout.minimumLineSpacing=space
+        }else{
+            layout.itemSize=CGSizeMake((hBounds-50-space*8)/7, (hBounds-50-space*8)/7)
+            layout.minimumInteritemSpacing=space
+            layout.minimumLineSpacing=space
+        }
+        
+        //ヘッダーのレイアウト
+        layout.headerReferenceSize=CGSizeMake(wBounds, 50)
+        
+        //callenderView生成
+        calendarCollectionView=UICollectionView(frame: CGRectMake(10, hNavigation,(wBounds-20),hBounds-hNavigation),collectionViewLayout:layout)
+        calendarCollectionView.tag = 100
+        
+        //カスタムセル（ペットボトル型）追加
+        calendarCollectionView.registerClass(CalendarCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        //ヘッダー追加
+        calendarCollectionView.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+        
+        calendarCollectionView.backgroundColor=UIColor.whiteColor()
+        calendarCollectionView.delegate=self
+        calendarCollectionView.dataSource=self
+        
+        return calendarCollectionView
+    }
+    
+    //月の最終日の取得
+    func getLastDay(year:Int,month:Int) -> Int{
+   
+        let calendar = NSCalendar.currentCalendar()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM";
+        let date:NSDate = dateFormatter.dateFromString("\(year)/\(month)")!;
+        let range = calendar.rangeOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.Month, forDate: date)
+        let dayCount = range.length
+        print("\(dayCount)")
+        
+        return dayCount;
+    }
+    
+    //月の初日の曜日を取得
+    func setUpDays(year:Int,month:Int) ->Int{
+        
+        let day:Int? = currentMonthLastDay
+        
+        if day != nil {
+            var weekday:Int = self.getWeekDay(year,month: month,day:1)
+            for var i:Int = 0; i < day!;i++ {
+                //var week:Int = self.getWeek(year,month: month,day:i+1)
+                weekday++
+                if weekday > 7 {
+                    weekday = 1
+                }
+            }
+            return weekday
+        }
+        return 0
+    }
+    
+    //曜日の取得
+    func getWeek(year:Int,month:Int,day:Int) ->Int{
+        let dateFormatter:NSDateFormatter = NSDateFormatter();
+        dateFormatter.dateFormat = "yyyy/MM/dd";
+        let date:NSDate? = dateFormatter.dateFromString(String(format:"%04d/%02d/%02d",year,month,day));
+        if date != nil {
+            let calendar:NSCalendar = NSCalendar.currentCalendar()
+            let dateComp:NSDateComponents = calendar.components(NSCalendarUnit.WeekOfMonth, fromDate: date!)
+            return dateComp.weekOfMonth;
+        }
+        return 0;
+    }
+    
+    //第何週の取得
+    func getWeekDay(year:Int,month:Int,day:Int) ->Int{
+        let dateFormatter:NSDateFormatter = NSDateFormatter();
+        dateFormatter.dateFormat = "yyyy/MM/dd";
+        let date:NSDate? = dateFormatter.dateFromString(String(format:"%04d/%02d/%02d",year,month,day));
+        if date != nil {
+            let calendar:NSCalendar = NSCalendar.currentCalendar()
+            let dateComp:NSDateComponents = calendar.components(NSCalendarUnit.Weekday, fromDate: date!)
+            return dateComp.weekday;
+        }
+        return 0;
+    }
+    
+    
+    //Cellに値を設定する
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell : CalendarCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CalendarCollectionViewCell
+        
+        //indexは0からのため
+        let day = indexPath.row-self.setUpDays(currentYear, month: currentMonth)-3
+        
+        
+        //
+        if day <= 0{
+            //先月の尻：12月
+            let lastMonthDay = self.getLastDay(currentYear, month: currentMonth-1) + day
+            cell.textLabel?.text=lastMonthDay.description
+            cell.iconImageView?.image=UIImage(named: "pet_back_ground_gray.png")
+        }else if day <= currentMonthLastDay{
+            
+            //今月
+            cell.textLabel?.text = day.description
+            //個数カウント
+            var number:Int=0
+            
+            for var n:Int = 0; n<pois.count; n++ {
+                
+                let poi = pois[n]
+                if Int(poi.date)==day{
+                    number++
+                }
+            }
+            if number==0 {
+                
+            }else{
+                cell.numberLabel!.text=number.description
+            }
+            
+            //今日
+            if day==currentDay {
+                todayPoi = number
+                cell.iconImageView?.image=UIImage(named: "pet_back_ground_orange.png")
+            }else{
+                //曜日ごとに色分け
+                if indexPath.row%7 == 0{
+                    cell.iconImageView?.image=UIImage(named: "pet_back_ground_pink.png")
+                }else if indexPath.row%7 == 6{
+                    cell.iconImageView?.image=UIImage(named: "pet_back_ground_gleen.png")
+                }else{
+                    cell.iconImageView?.image=UIImage(named: "pet_back_ground_blue.png")
+                }
+            }
+        }else{
+            //来月の頭
+            let nextMonthDay = day-currentMonthLastDay
+            cell.textLabel?.text=nextMonthDay.description
+            cell.iconImageView?.image=UIImage(named: "pet_back_ground_gray.png")
+        }
+        
+        return cell
+    }
+    
+    //Cellのヘッダー
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        
+        let headerReusableView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "header", forIndexPath: indexPath)
+        let todayLabel:UILabel = UILabel(frame: CGRectMake(0, 0,wBounds, 50))
+        todayLabel.text="- \(currentMonth)月 -"
+        todayLabel.textColor=UIColor.grayColor()
+        todayLabel.font=UIFont.systemFontOfSize(30)
+        todayLabel.textAlignment=NSTextAlignment.Center
+      
+        headerReusableView.addSubview(todayLabel)
+        
+        return headerReusableView
+    }
+    
+    //Cellが選択
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("Num: \(indexPath.row)")
+    }
+    
+    //Cellの総数を返す
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 42
+    }
+    
+    
+    
+    //XML解析
+    var pois : [Poi]=[Poi]()
+    
+    class Poi {
+        var id:String!
+        var date:String!
+    }
+    
+    var currentElementName : String?
+    
+    let poiElementName="poi"
+    let idElementName="id"
+    let dateElementName="day"
+    
+    func parserDidStartDocument(parser: NSXMLParser) {
+        print("解析開始")
+    }
+
+    func parser(parser: NSXMLParser,
+        didStartElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName: String?,
+        attributes attributeDict: [String : String])
+    {
+        currentElementName = nil
+        if elementName == poiElementName {
+            pois.append(Poi())
+        } else {
+            currentElementName = elementName
+        }
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String)
+    {
+        if pois.count > 0 {
+            let lastPoi = pois[pois.count-1]
+            if currentElementName == idElementName {
+                let tmpString : String? = lastPoi.id
+                lastPoi.id = (tmpString != nil) ? tmpString! + string : string
+                print("ID:\(lastPoi.id)")
+            } else if currentElementName == dateElementName {
+                lastPoi.date = string
+                print("DATE:\(lastPoi.date)")
+            }
+        }
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
+    {
+        currentElementName = nil;
+    }
+    
+    func parserDidEndDocument(parser: NSXMLParser)
+    {
+        print("解析終了")
+    }
+    
+    @IBAction func refreshButton(sender: AnyObject) {
+        
+        pois=[]
+        
+        //サーバーURL
+        let data:NSData? = NSData(contentsOfURL: NSURL(string: "http://210.140.67.223/poilog.php?name=kimura1&pass=kimu1")!)
+        //パーサー用意、接続開始
+        let parser : NSXMLParser! = NSXMLParser(data: data!)
+        print("通信開始")
+        if parser != nil{
+            parser!.delegate=self
+            parser!.parse()
+        }else{
+            print("false")
+        }
+        
+        //カレンダービュー消去・追加
+        let remove = self.view.viewWithTag(100)
+        remove!.removeFromSuperview()
+        //self.view.addSubview(calendarView())
+    }
+    
+    //今日のんだ本数View
+    private func todayView()->UIView{
+        let todayView=UIView(frame: CGRectMake(0, hNavigation,wBounds,hBounds-hNavigation))
+        
+        let todayLabel:UILabel = UILabel(frame: CGRectMake(20, 0,wBounds-40, hBounds/2-10))
+        todayLabel.text="- TOTAL -"
+        todayLabel.textColor=UIColor.grayColor()
+        todayLabel.font=UIFont.systemFontOfSize(30)
+        todayLabel.textAlignment=NSTextAlignment.Center
+        
+        //背景
+        let petImage:UIImage = UIImage(named: "pet_back_ground_pink.png")!
+        let petImageView:UIImageView = UIImageView(frame:  CGRectMake(35, 55, wBounds-70, hBounds))
+        petImageView.contentMode=UIViewContentMode.ScaleAspectFit
+        petImageView.image=petImage
+        //本数のラベル
+        let todayNumberLabel:UILabel = UILabel(frame: CGRectMake(20, 80,wBounds-40, hBounds))
+        todayNumberLabel.text=pois.count.description
+        todayNumberLabel.textColor=UIColor.whiteColor()
+        todayNumberLabel.font=UIFont.systemFontOfSize(100)
+        //todayNumberLabel.font=UIFont.boldSystemFontOfSize(UIFont.labelFontSize())
+        todayNumberLabel.textAlignment=NSTextAlignment.Center
+        //ぽい
+        let poiLabel:UILabel = UILabel(frame: CGRectMake(90, 110,wBounds-40, hBounds))
+        poiLabel.text="Poi"
+        poiLabel.textColor=UIColor.whiteColor()
+        poiLabel.font=UIFont.systemFontOfSize(20)
+        poiLabel.font=UIFont.boldSystemFontOfSize(UIFont.labelFontSize())
+        poiLabel.textAlignment=NSTextAlignment.Center
+        
+        //値段
+        let priceLabel:UILabel = UILabel(frame: CGRectMake(20, 160,wBounds-40, hBounds))
+        let price:Int = pois.count*150
+        priceLabel.text="x 150 = \(price) yen"
+        priceLabel.textColor=UIColor.whiteColor()
+        priceLabel.font=UIFont.systemFontOfSize(20)
+        priceLabel.textAlignment=NSTextAlignment.Center
+        
+        todayView.addSubview(todayLabel)
+        todayView.addSubview(petImageView)
+        todayView.addSubview(todayNumberLabel)
+        todayView.addSubview(poiLabel)
+        todayView.addSubview(priceLabel)
+        todayView.backgroundColor=UIColor.whiteColor()
+        return todayView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
